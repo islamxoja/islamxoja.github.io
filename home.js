f845be10018bfabe22c12c8a349798014c16d0b5
islamxoja <islamxojayev@gmail.com>
2023-02-05 23:38:09 +0500

    first

---
+++ home.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    alert("Your order successifuly accepted!");
+}
+function myReset() {
+    document.getElementById("form1").reset();
+}
+function myOrder() {
+    document.getElementById("order").innerHTML = "Your order is done!";
+}
